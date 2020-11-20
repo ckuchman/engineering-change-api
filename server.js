8ee0efa9df293cd632f0403b248ac5c86ba82672
Chris <kuch517@gmail.com>
2020-11-20 07:49:10 -0800

    Added methods from other assignment to flesh out

--- server.js
+++ server.js
@@ -43,8 +43,7 @@ function insertEntry(newEntry, objectName) {
   return new Promise(resolve => {
     datastore.insert(newEntry, (err, apiResponse) => {
       if (err) {
-        console.log(err);
-        result.status = 400;
+        result.status = 500;
         result.error = "Failed to insert " + objectName;
       } else {
         result.status = 201;
@@ -106,8 +105,34 @@ function getEntryList(req, query, urlPath) {
           entity.self = req.protocol + "://" + req.get("host") + urlPath + entity[datastore.KEY].id;
         });
 
+        // Add indicators for pagination
+        if (info.moreResults != datastore.NO_MORE_RESULTS) {
+          result.cursor = req.protocol + "://" + req.get("host") + urlPath + "?cursor=" + info.endCursor;
+        }
+
         result.status = 200;
         result.entities = entities;
+      }
+      resolve(result);
+    });
+  });
+}
+
+
+// Modifies the entry in the Datastore and return error results
+function editEntry(updatedEntity, objectName) {
+  var result = {
+    'status': null,
+    'error': null
+  };
+
+  return new Promise(resolve => {
+    datastore.update(updatedEntity, (err, apiResponse) => {
+      if (err) {
+        result.status = 404;
+        result.error = "No " + objectName + " with this " + objectName + "_id exists";
+      } else {
+        result.status = 200;
       }
       resolve(result);
     });
@@ -125,7 +150,7 @@ function deleteEntry(entryKey, objectName) {
   return new Promise(resolve => {
     datastore.delete(entryKey, (err, apiResponse) => {
       if (err ||apiResponse.indexUpdates == 0) {
-        result.status = 403;
+        result.status = 404;
         result.error = "No " + objectName + " with this " + objectName + "_id exists";
       } else {
         result.status = 204;
@@ -167,6 +192,53 @@ async function createEntry(req, res, newEntry, objectName, urlPath) {
 }
 
 
+// Gets the entry list with pagination and sends a response back with the entries information.
+async function getEntryListAndRespond(req, res, kind, urlPath, paginationLimit) {
+  const query = datastore.createQuery(kind).limit(paginationLimit);
+
+  if (req.query.cursor) {
+    query.start(req.query.cursor);
+  }
+
+  const entryList = await getEntryList(req, query, urlPath);
+
+  // Respond with entities list or error message
+  if (entryList.error == null) {
+    res.status(entryList.status).json({
+      'entities': entryList.entities,
+      'next': entryList.cursor
+    });
+  } else {
+    res.status(entryList.status).json({'Error': entryList.error});
+  }
+}
+
+/////////////////// General Functions ///////////////////
+
+// Checks if the passed key has the matching attribute
+async function checkDuplicate(req, kind, attr, value) {
+
+  const query = datastore.createQuery(kind).filter(attr, '=', value);
+
+  // Get entry from Datastore
+  const entryList = await getEntryList(req, query, "");
+
+  var response = {
+    error: null,
+    duplicate: null
+  }
+
+  if (entryList.error != null) {
+    response.error = entryList.error;
+  } else if (entryList.entities.length > 0) {
+    response.duplicate = true;
+  } else {
+    response.duplicate = false;
+  }
+
+  return response;
+}
+
 /////////////////// Routing ///////////////////
 
 
@@ -202,6 +274,59 @@ app.get("/oauth", async (req, res) => {
 // Create a Boat
 app.post('/boats', async (req, res) => {
 
+  // // Validate content type
+  // if (req.get('Content-Type') != 'application/json') {
+  //   res.status(415).json({
+  //     "Error": "The request uses a unsupported media type"
+  //   });
+  //   return;
+  // }
+
+  // // Validate input
+  // containsValidName = false;
+  // containsValidType = false;
+  // containsValidLength = false;
+  // containsNonvalid = false;
+
+  // for (prop in req.body) {
+  //   if (prop == 'name') {
+  //     containsValidName = true;
+  //   } else if (prop == 'type') {
+  //     containsValidType = true;
+  //   } else if (prop == 'length' && typeof req.body[prop] == 'number') {
+  //     containsValidLength = true;
+  //   } else {
+  //     containsNonvalid = true;
+  //   }
+  // }
+
+  //   // Return error if bad input
+  // if (!(containsValidName &&
+  //       containsValidType &&
+  //       containsValidLength)
+  //     ||
+  //     containsNonvalid) {
+  //   res.status(400).json({
+  //     "Error": "The request object is missing at least one of the required attributes, contains not allowed information, or the attribute has the wrong value type"
+  //   });
+  //   return;
+  // }
+
+  // // Check for duplicates
+  // var duplicateResponse = await checkDuplicate(req, 'Boat', 'name', req.body.name);
+
+  // if (duplicateResponse.error != null) {
+  //   res.status(400).json({
+  //     "Error": "Falied to determine if the name was a duplicate"
+  //   });
+  //   return;
+  // } else if (duplicateResponse.duplicate == true) {
+  //   res.status(403).json({
+  //     "Error": "The request uses a already existing name"
+  //   });
+  //   return;
+  // }
+
   // Check the JWT value
   var ticket
   try {
@@ -233,6 +358,48 @@ app.post('/boats', async (req, res) => {
 
   // Create entry and respond with values
   createEntry(req, res, boatEntity, "boat", "/boats/");
+});
+
+
+// Get a Boat
+app.get('/boats/:boat_id', async (req, res) => {
+
+  // Validate content request
+  if (req.get('Accept') != '*/*' && req.get('Accept') != 'application/json' && req.get('Accept') != 'text/html') {
+    res.status(406).json({
+      "Error": "The request asks for an unsupported media type"
+    });
+    return;
+  }
+
+  // Generate Key
+  const key = datastore.key(['Boat', Number(req.params.boat_id)]);
+
+  var response = {
+    status: null,
+    content: {}
+  };
+
+  // Get entry from Datastore
+  const entryData = await getEntry(req, key, "boat", "/boats/");
+
+  // Respond with entity data or error message
+  if (entryData.error != null) {
+    res.status(entryData.status).json({'Error': entryData.error});
+  } else {
+    if (req.get('Accept') == '*/*' || req.get('Accept') == 'application/json') {
+      res.status(entryData.status).json(entryData.entity);
+    } else {
+      // Construct HTML response
+      htmlRes = '<ul>';
+      for (attr in entryData.entity) {
+        htmlRes = htmlRes + '<li>' + attr + ': ' + entryData.entity[attr] + '</li>';
+      }
+      htmlRes = htmlRes + '</ul>';
+
+      res.status(entryData.status).send(htmlRes);
+    }
+  }
 });
 
 
@@ -324,6 +491,220 @@ app.delete('/boats/:boat_id', async (req, res) => {
   }
 });
 
+
+// Update a Boat
+app.put('/boats/:boat_id', async (req, res) => {
+
+  // Validate content type
+  if (req.get('Content-Type') != 'application/json') {
+    res.status(415).json({
+      "Error": "The request uses a unsupported media type"
+    });
+    return;
+  }
+  
+  // Validate content request
+  if (req.get('Accept') != '*/*' && req.get('Accept') != 'application/json') {
+    res.status(406).json({
+      "Error": "The request asks for an unsupported media type"
+    });
+    return;
+  }
+
+  // Validate input
+  containsValidName = false;
+  containsValidType = false;
+  containsValidLength = false;
+  containsNonvalid = false;
+
+  for (prop in req.body) {
+    if (prop == 'name') {
+      containsValidName = true;
+    } else if (prop == 'type') {
+      containsValidType = true;
+    } else if (prop == 'length' && typeof req.body[prop] == 'number') {
+      containsValidLength = true;
+    } else {
+      containsNonvalid = true;
+    }
+  }
+
+  // Return error if bad input
+  if (!(containsValidName &&
+        containsValidType &&
+        containsValidLength)
+      ||
+      containsNonvalid) {
+    res.status(400).json({
+      "Error": "The request object is missing at least one of the required attributes, contains not allowed information, or the attribute has the wrong value type"
+    });
+    return;
+  }
+
+  // Check for duplicates
+  var duplicateResponse = await checkDuplicate(req, 'Boat', 'name', req.body.name);
+
+  if (duplicateResponse.error != null) {
+    res.status(400).json({
+      "Error": "Falied to determine if the name was a duplicate"
+    });
+    return;
+  } else if (duplicateResponse.duplicate == true) {
+    res.status(403).json({
+      "Error": "The request uses a already existing name"
+    });
+    return;
+  }
+
+  // Prepare the updated entity
+  const boat = {
+    key: datastore.key(['Boat', Number(req.params.boat_id)]),
+    data: {
+      name: req.body.name,
+      type: req.body.type,
+      length: req.body.length,
+    },
+  }
+
+  const editResponse = await editEntry(boat, "boat");
+
+  // Respond with entities list or error message
+  if (editResponse.error == null) {
+    // Populate return entity
+    entity = boat.data
+    entity.id = req.params.boat_id;
+    entity.self = req.protocol + "://" + req.get("host") + "/boats/" + req.params.boat_id;
+    res.status(303).set("Location", entity.self).json(entity);
+  } else {
+    res.status(editResponse.status).json({'Error': editResponse.error});
+  }
+});
+
+
+// Edit a Boat
+app.patch('/boats/:boat_id', async (req, res) => {
+
+  // Validate content type
+  if (req.get('Content-Type') != 'application/json') {
+    res.status(415).json({
+      "Error": "The request uses a unsupported media type"
+    });
+    return;
+  }
+  
+  // Validate content request
+  if (req.get('Accept') != '*/*' && req.get('Accept') != 'application/json') {
+    res.status(406).json({
+      "Error": "The request asks for an unsupported media type"
+    });
+    return;
+  }
+
+  // Validate input
+  containsValidName = false;
+  containsValidType = false;
+  containsValidLength = false;
+  containsNonvalid = false;
+
+  for (prop in req.body) {
+    if (prop == 'name') {
+      containsValidName = true;
+    } else if (prop == 'type') {
+      containsValidType = true;
+    } else if (prop == 'length' && typeof req.body[prop] == 'number') {
+      containsValidLength = true;
+    } else {
+      containsNonvalid = true;
+    }
+  }
+
+  // Return error if bad input
+  if (!(containsValidName ||
+        containsValidType ||
+        containsValidLength)
+      ||
+      containsNonvalid) {
+    res.status(400).json({
+      "Error": "The request object has no valid attributes, contains not allowed information, or the attribute has the wrong value type"
+    });
+    return;
+  }
+
+  // Check for duplicates if name changed
+  if (containsValidName) {
+    var duplicateResponse = await checkDuplicate(req, 'Boat', 'name', req.body.name);
+
+    if (duplicateResponse.error != null) {
+      res.status(400).json({
+        "Error": "Falied to determine if the name was a duplicate"
+      });
+      return;
+    } else if (duplicateResponse.duplicate == true) {
+      res.status(403).json({
+        "Error": "The request uses a already existing name"
+      });
+      return;
+    }
+  }
+
+  // Get old from Datastore
+  const key = datastore.key(['Boat', Number(req.params.boat_id)]);
+  const oldData = await getEntry(req, key, "boat", "/boats/");
+
+  // Update to new information
+  if (containsValidName) {
+    nameVal = req.body.name;
+  } else {
+    nameVal = oldData.entity.name;
+  }
+
+  if (containsValidType) {
+    typeVal = req.body.type;
+  } else {
+    typeVal = oldData.entity.type;
+  }
+
+  if (containsValidLength) {
+    lengthVal = req.body.length;
+  } else {
+    lengthVal = oldData.entity.length;
+  }
+
+  // Prepare the updated entity
+  const boat = {
+    key: datastore.key(['Boat', Number(req.params.boat_id)]),
+    data: {
+      name: nameVal,
+      type: typeVal,
+      length: lengthVal,
+    },
+  }
+
+  const editResponse = await editEntry(boat, "boat");
+
+  // Respond with entities list or error message
+  if (editResponse.error == null) {
+    // Populate return entity
+    entity = boat.data
+    entity.id = req.params.boat_id;
+    entity.self = req.protocol + "://" + req.get("host") + "/boats/" + req.params.boat_id;
+    res.status(editResponse.status).json(entity);
+  } else {
+    res.status(editResponse.status).json({'Error': editResponse.error});
+  }
+});
+
+
+// Edit the Boat List
+app.put('/boats', async (req, res) => {
+  res.status(405).json({'Error': 'The request asks for an unsupported method'});
+});
+
+
+// Delete the Boat List
+app.delete('/boats', async (req, res) => {
+  res.status(405).json({'Error': 'The request asks for an unsupported method'});
+});
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
